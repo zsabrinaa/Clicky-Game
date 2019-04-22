2a873a1dbdf98c59fccc49073ba94044eaab67de
Sabrina <zsbrina20@gmail.com>
2019-04-21 23:38:23 -0500

    test

--- src/clicky-game/src/components/Nav.js
+++ src/clicky-game/src/components/Nav.js
@@ -6,7 +6,7 @@ function NavBar(props) {
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">High Score:{props.highScore}Your Score:{props.score}</span>
+        <span className="navbar-brand mb-0 h1">High Score:{props.highScore} <br></br>Your Score:{props.score}</span>
       </nav>
     </div>
 
